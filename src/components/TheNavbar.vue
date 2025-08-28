5f314a0eeabb7f70278eb7b08754c238e5b2c602
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2025-08-28 16:57:40 -0400

    more updates
    -favicon
    -variables.scss
    -homeview

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -3,18 +3,18 @@
 		<div class="container">
 			<router-link to="/" class="logo">joeysteel.com</router-link>
 			<ul class="nav-links">
-				<li><router-link to="/about">about</router-link></li>
-				<li><router-link to="/projects">projects</router-link></li>
-				<li><router-link to="/contact">contact</router-link></li>
+				<li><router-link to="/about" class="hover-white-blue-gradient">about</router-link></li>
+				<li>
+					<router-link to="/projects" class="hover-white-blue-gradient">projects</router-link>
+				</li>
+				<li><router-link to="/contact" class="hover-white-blue-gradient">contact</router-link></li>
 			</ul>
 		</div>
 	</nav>
 </template>
 
 <style scoped lang="scss">
-$primary-color: #71d7ff;
-$text-color: #fff;
-$background-color: #333;
+@use "../assets/variables" as *;
 
 .navbar {
 	// Sticky and full-width properties
@@ -35,8 +35,11 @@ $background-color: #333;
 
 		.logo {
 			color: $text-color;
-			font-size: 1.5rem;
+			font-size: 1.75rem;
 			text-decoration: none;
+			&:hover {
+				color: $text-lighter;
+			}
 		}
 
 		.nav-links {
@@ -44,25 +47,10 @@ $background-color: #333;
 			display: flex;
 			margin: 0;
 			padding: 0;
+			font-size: 1.1rem;
 
 			li {
 				margin-left: 20px;
-			}
-		}
-
-		a {
-			text-decoration: none;
-			color: transparent; // Make the text color transparent
-			background-image: linear-gradient(to right, $text-color, $primary-color); // Your two colors
-			background-size: 200% 100%; // Double the width
-			background-position: 0 0; // Position to show the first color
-			-webkit-background-clip: text;
-			-webkit-text-fill-color: transparent;
-			background-clip: text;
-			transition: background-position 0.3s ease; // Animate the position
-
-			&:hover {
-				background-position: 100% 0; // Slide the gradient over
 			}
 		}
 	}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,46 @@
-<script setup></script>
-
 <template>
-	<div>Hello world.</div>
+	<div class="home-page">
+		<h1>Hello. I'm <span class="joey hover-black-blue-gradient">Joey.</span></h1>
+		<ul class="nav-links">
+			<li><router-link to="/about" class="hover-black-blue-gradient">developer.</router-link></li>
+			<li><router-link to="/projects" class="hover-black-blue-gradient">designer.</router-link></li>
+			<li><router-link to="/contact" class="hover-black-blue-gradient">artist.</router-link></li>
+		</ul>
+	</div>
 </template>
+
+<style lang="scss" scoped>
+@use "../assets/variables" as *;
+.home-page {
+	min-height: 100vh;
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+
+	h1 {
+		font-size: 5em;
+		color: $background-color;
+		width: 100%;
+		.joey {
+			&:hover {
+				cursor: default;
+			}
+		}
+	}
+
+	.nav-links {
+		list-style: none;
+		display: flex;
+		margin: 0;
+		padding: 0;
+
+		li {
+			font-size: 2rem;
+			margin-left: 20px;
+			&:first-child {
+				margin-left: 0;
+			}
+		}
+	}
+}
+</style>
